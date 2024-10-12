26151e3b7371a1e68def5ff6f7df648c6eb1730c
Jing Tao <tao@nceas.ucsb.edu>
2024-10-11 17:22:59 -0700

    Add a new class representing the osti v2 xml service.

---
+++ src/main/java/edu/ucsb/nceas/osti_elink/v2/xml/OSTIv2XmlService.java
@@ -0,0 +1,53 @@
+package edu.ucsb.nceas.osti_elink.v2.xml;
+
+import edu.ucsb.nceas.osti_elink.OSTIElinkService;
+import edu.ucsb.nceas.osti_elink.OSTIServiceFactory;
+import edu.ucsb.nceas.osti_elink.exception.PropertyNotFound;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+/**
+ * @author Tao
+ * This class represents the service for v2xml
+ */
+public class OSTIv2XmlService extends OSTIElinkService {
+    public static final String OSTI_TOKEN = "OSTI_TOKEN";
+    private static String token;
+
+    /**
+     * Constructor
+     *
+     * @param username the username of the account which can access the OSTI service
+     * @param password the password of the account which can access the OSTI service
+     * @param baseURL  the url which specifies the location of the OSTI service
+     * @throws PropertyNotFound
+     * @throws IOException
+     */
+    public OSTIv2XmlService(String username, String password, String baseURL)
+        throws PropertyNotFound, IOException {
+        super(username, password, baseURL);
+        loadToken();
+    }
+
+    /**
+     * This method loads the token from the environmental variable OSTI_TOKEN first.
+     * If this environment variable is not defined, it will read the token file path from the
+     * property file and load the token from that file.
+     * @throws PropertyNotFound
+     * @throws IOException
+     */
+    protected void loadToken() throws PropertyNotFound, IOException {
+        token = System.getenv(OSTI_TOKEN);
+        if (token == null) {
+            String token_path = OSTIServiceFactory.getProperty("ostiService.tokenPath", properties);
+            log.debug("Can't get the token from the environmental variable OSTI_TOKEN and will "
+                          + "read it from a file " + token_path);
+            token = FileUtils.readFileToString(new File(token_path), "UTF-8");
+        } else {
+            log.debug("It got the token from the environmental variable OSTI_TOKEN.");
+        }
+    }
+}
