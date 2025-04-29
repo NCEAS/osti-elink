682536a020c7dbd38d5fa1dafc6fc7b1ae2936e4
Shalki Shrivastava <shrivastava.shal@gmail.com>
2025-04-29 14:07:14 -0500

    wip: un-related test failing: OSTIElinkClientV2XmlTest.testSetAndGetMetadataAndPublish:169 expected:<[R]> but was:<[SV]>

--- src/main/java/edu/ucsb/nceas/osti_elink/v2/json/OSTIv2JsonService.java
+++ src/main/java/edu/ucsb/nceas/osti_elink/v2/json/OSTIv2JsonService.java
@@ -1,9 +1,12 @@
 package edu.ucsb.nceas.osti_elink.v2.json;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import edu.ucsb.nceas.osti_elink.OSTIElinkException;
+import edu.ucsb.nceas.osti_elink.OSTIElinkNotFoundException;
 import edu.ucsb.nceas.osti_elink.OSTIElinkService;
 import edu.ucsb.nceas.osti_elink.OSTIServiceFactory;
 import edu.ucsb.nceas.osti_elink.exception.PropertyNotFound;
+import edu.ucsb.nceas.osti_elink.v2.response.JsonResponseHandler;
 import org.apache.commons.io.FileUtils;
 import org.apache.http.client.methods.HttpUriRequest;
 
@@ -13,6 +16,7 @@ import java.util.Properties;
 
 public class OSTIv2JsonService extends OSTIElinkService {
 
+    public static final String WORKFLOW_STATUS = "workflow_status";
     public static final String DOI_QUERY_MAX_ATTEMPTS_ENV_NAME =
             "METACAT_OSTI_DOI_QUERY_MAX_ATTEMPTS";
     public static final String OSTI_TOKEN_ENV_NAME = "METACAT_OSTI_TOKEN";
@@ -66,9 +70,72 @@ public class OSTIv2JsonService extends OSTIElinkService {
             }
 
         }
+    }
+
+    /**
+     * Get the status of a DOI. If there are multiple records for a DOI, the status of
+     * the first one will be returned
+     * @param doi  the doi to identify the record
+     * @return  the status of the doi
+     * @throws OSTIElinkException
+     */
+    @Override
+    public String getStatus(String doi) throws OSTIElinkException {
+        String status;
+        String metadata = null;
+        long start = System.currentTimeMillis();
+        for (int i = 0; i <= maxAttempts; i++ ) {
+            try {
+                metadata = getMetadata(doi);
+                break;
+            } catch (OSTIElinkNotFoundException e) {
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException ex) {
+                    log.warn("The thread waiting for the DOI searchable in the getStatus method "
+                            + "was interrupted " + ex.getMessage());
+                }
+            }
+            if (i >= maxAttempts) {
+                throw new OSTIElinkNotFoundException("The library tried " + maxAttempts + " times"
+                        + " to query the status of " + doi + " "
+                        + "from the OSTI service. However OSTI "
+                        + "service still can't find it");
+            }
+        }
+        long end = System.currentTimeMillis();
+        log.warn("It waited " + (end - start)/1000 + " seconds for doi " + doi + " to be "
+                + "searchable after minting it.");
+        try {
+            status = JsonResponseHandler.getPathValue(metadata, WORKFLOW_STATUS);
+        } catch (JsonProcessingException e) {
+            throw new OSTIElinkException(e.getMessage());
+        }
+        if (status == null) {
+            throw new OSTIElinkException("There is no workflow_status for " + doi +" in the query"
+                    + " result:\n" + metadata);
+        }
+        // Our metacat checks the status of Saved. However, from v1 to v2, the status of Saved
+        // changed to SA. In order not to change Metacat's code, we need to map SA to Saved
+        if (status.equals("SA")) {
+            status = OSTIElinkService.SAVED;
+        }
+        log.debug("The status of " + doi + " is " + status);
+        return status;
+    }
 
 
-
+    /**
+     * Get the metadata associated with the given identifier. An OSTIElinkNotFoundException
+     * will be thrown if the identifier can't be found.
+     * @param identifier  the identifier for which the metadata should be returned
+     * @param  type  the type of the identifier, which can be doi or OSTIId
+     * @return  the metadata in the xml format
+     * @throws OSTIElinkException
+     */
+    @Override
+    protected String getMetadata(String identifier, String type) throws OSTIElinkException {
+        return "";
     }
 
     protected void loadToken() throws PropertyNotFound, IOException {
